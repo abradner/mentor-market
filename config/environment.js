5f7cd4cb5ccf04d2ca05dade47a1eaf17c7018ea
Alexander Bradner <alex@bradner.net>
2014-12-20 18:42:39 +1100

    Initial setup: moving to pods, using ember(-data) canary and associated libs (htmlbars, jquery)

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'mentor-market',
+    podModulePrefix: 'mentor-market/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
